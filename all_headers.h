4dbe0f83e9fff64ea1168af5bdeb20aa0a1caccd
Vijay shamra <sharmavijay1991@gmail.com>
2013-06-24 00:35:27 +0530

    Create all_headers.h

---
+++ all_headers.h
@@ -0,0 +1,24 @@
+/*------------------------------------------------------------------
+This file is used to hold all header files in a single file.
+Basically we can say that this file will hold :-
+* Basic c++ library.
+* IPV4 library.
+* IPV6 library.
+Created By Vijay Sharma(sharmavijay1991@gmail.com)
+--------------------------------------------------------------------*/
+
+// IPV4 files
+#include <net/if.h>
+// IPV6 files
+#include <netinet/in.h>
+
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdio.h>
+//#include <conio.h>
+//#include <sys/type.h>
+#include <unistd.h>
+#include "string"
+#include <stdlib.h>
+#include <errno.h>    
